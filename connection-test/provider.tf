1bd1c3e4760ddd3bb85617cde95cf25f7763c1c6
Fabrice Semti <fabrice@fabricesemti.com>
2024-11-13 16:20:24 +0000

    Update provider.tf

--- connection-test/provider.tf
+++ connection-test/provider.tf
@@ -16,6 +16,24 @@ provider "azurerm" {
   client_secret   = var.client_secret
 }
 
-variable "subscription_id" {}
-variable "tenant_id" {}
-variable "client_id" {}
+variable "subscription_id" {
+  type        = string
+  description = "Azure Subscription ID"
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Azure Tenant ID"
+}
+
+variable "client_id" {
+  type        = string
+  description = "Azure Client ID"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "Azure Client Secret"
+  sensitive   = true
+}
+
