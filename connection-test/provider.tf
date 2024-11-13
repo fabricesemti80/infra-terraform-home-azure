31b23abb42b671d0b1275df0f9430a5526e9af87
Fabrice Semti <fabrice@fabricesemti.com>
2024-11-13 16:19:47 +0000

    Update provider.tf

--- connection-test/provider.tf
+++ connection-test/provider.tf
@@ -10,10 +10,10 @@ terraform {
 }
 provider "azurerm" {
   features {}
-  use_msi         = true
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
   client_id       = var.client_id
+  client_secret   = var.client_secret
 }
 
 variable "subscription_id" {}
